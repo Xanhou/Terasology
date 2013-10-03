fc95547627027edd34ff153585bf76f3db2e58b7
Xanhou <xanhou.terasology@gmail.com>
2013-10-03 08:03:41 +0200

    Switch pcs update

--- src/main/java/org/terasology/logic/characters/movement/KinematicCharacterMover.java
+++ src/main/java/org/terasology/logic/characters/movement/KinematicCharacterMover.java
@@ -105,68 +105,8 @@ public class KinematicCharacterMover implements CharacterMover {
      */
     private void checkMode(final CharacterMovementComponent movementComp, final CharacterStateEvent state,
             final CharacterStateEvent oldState, EntityRef entity, boolean firstRun) {
-        //If we are ghosting, the mode cannot be changed.
-        if (state.getMode() == MovementMode.GHOSTING) {
-            return;
-        }
-
-        Vector3f worldPos = state.getPosition();
-        Vector3f top = new Vector3f(worldPos);
-        Vector3f bottom = new Vector3f(worldPos);
-        top.y += 0.25f * movementComp.height;
-        bottom.y -= 0.25f * movementComp.height;
-
-        final boolean topUnderwater = worldProvider.getBlock(top).isLiquid();
-        final boolean bottomUnderwater = worldProvider.getBlock(bottom).isLiquid();
-
-        final boolean newSwimming = topUnderwater && bottomUnderwater;
-        boolean newClimbing = false;
-
-        //TODO: refactor this knot of if-else statements into something easy to read. Some sub-methods and switch statements would be nice.
-        if (!newSwimming) {
-            Vector3f[] sides = {new Vector3f(worldPos), new Vector3f(worldPos), new Vector3f(worldPos), new Vector3f(
-                worldPos), new Vector3f(worldPos)};
-            float factor = 0.18f;
-            sides[0].x += factor * movementComp.radius;
-            sides[1].x -= factor * movementComp.radius;
-            sides[2].z += factor * movementComp.radius;
-            sides[3].z -= factor * movementComp.radius;
-            sides[4].y -= movementComp.height;
-            for (Vector3f side : sides) {
-                if (worldProvider.getBlock(side).isClimbable()) {
-                    //If any of our sides are near a climbable block, climb!
-                    newClimbing = true;
-                    break;
-                }
-            }
-        }
-
-        if (newSwimming) {
-            //Note that you cannot climb under water!
-            if (state.getMode() != MovementMode.SWIMMING) {
-                if (firstRun) {
-                    entity.send(new OnEnterLiquidEvent(worldProvider.getBlock(state.getPosition())));
-                }
-                state.setMode(MovementMode.SWIMMING);
-            }
-        } else if (state.getMode() == MovementMode.SWIMMING) {
-            if (firstRun) {
-                entity.send(new OnLeaveLiquidEvent(worldProvider.getBlock(oldState.getPosition())));
-            }
-            if (newClimbing) {
-                state.setMode(MovementMode.CLIMBING);
-                state.getVelocity().y = 0;
-            } else {
-                if (state.getVelocity().y > 0) {
-                    state.getVelocity().y += 8;
-                }
-                state.setMode(MovementMode.WALKING);
-            }
-        } else if (newClimbing != (state.getMode() == MovementMode.CLIMBING)) {
-            //We need to toggle the climbing mode
-            state.getVelocity().y = 0;
-            state.setMode((newClimbing) ? MovementMode.CLIMBING : MovementMode.WALKING);
-        }
+        UpdateMovementModeEvent event = new UpdateMovementModeEvent(oldState, state, firstRun);
+        entity.send(event);
     }
 
     private Vector3f extractResidualMovement(Vector3f hitNormal, Vector3f direction) {
